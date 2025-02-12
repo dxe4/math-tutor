bf959a4df5ced755b122b94efd86b6ce2b05249a
charalmapos papaloizou <harry@e2e4.email>
2025-02-12 05:55:07 +0000

    start backend

---
+++ math_tutor_backend/math_base/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MathBaseConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "math_base"
---
+++ math_tutor_backend/math_base/models.py
@@ -0,0 +1,57 @@
+from datetime import datetime
+from zoneinfo import ZoneInfo
+
+from django.contrib.auth.models import (
+    AbstractBaseUser, BaseUserManager, PermissionsMixin
+)
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from model_utils.models import TimeStampedModel
+from typing import TYPE_CHECKING
+
+UTC = ZoneInfo("UTC")
+
+
+def utc_now():
+    return datetime.now(UTC)
+
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError(_("The Email field must be set"))
+        email = self.normalize_email(email)
+        extra_fields.setdefault("is_active", True)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if not extra_fields.get("is_staff"):
+            raise ValueError(_("Superuser must have is_staff=True."))
+        if not extra_fields.get("is_superuser"):
+            raise ValueError(_("Superuser must have is_superuser=True."))
+
+        return self.create_user(email, password, **extra_fields)
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email: str = models.EmailField(unique=True)
+    username: str = models.CharField(max_length=150, unique=True)
+
+    is_active: bool = models.BooleanField(default=True)
+    is_staff: bool = models.BooleanField(default=False)
+    created_at: datetime = models.DateTimeField(auto_now_add=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
+
+    def __str__(self):
+        return str(self.email)
