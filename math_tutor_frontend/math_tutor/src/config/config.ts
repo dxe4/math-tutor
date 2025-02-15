575e2ebca7c23c8d5a53a523a49e5fa84555b6d5
charalmapos papaloizou <harry@e2e4.email>
2025-02-15 09:07:46 +0000

    fix base url

--- math_tutor_frontend/math_tutor/src/config/config.ts
+++ math_tutor_frontend/math_tutor/src/config/config.ts
@@ -1,5 +1,5 @@
 const config = {
-    API_HOST: process.env.REACT_APP_API_HOST || "http://http://localhost:8000/",
+  API_HOST: process.env.REACT_APP_API_HOST || "http://localhost:8000",
 };
 
 export default config;
