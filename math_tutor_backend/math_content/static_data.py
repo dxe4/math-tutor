458ad3348e40c8c07a0b687da782e4e14e5fd7c1
charalmapos papaloizou <harry@e2e4.email>
2025-02-16 15:49:49 +0000

    add books

---
+++ math_tutor_backend/math_content/static_data.py
@@ -0,0 +1,102 @@
+BASE_BOOKS = {
+    "Abstract Algebra": [
+        {
+            "title": "Abstract Algebra",
+            "author": "David S. Dummit, Richard M. Foote",
+            "isbn": "9780471433347",
+            "link": "https://www.goodreads.com/book/show/264543.Abstract_Algebra",
+        },
+    ],
+    "Algebra": [
+        {
+            "title": "Algebra",
+            "author": "Serge Lang",
+            "isbn": "9780387953854",
+            "link": "https://www.goodreads.com/book/show/79780.Algebra",
+        },
+    ],
+    "Calculus": [
+        {
+            "title": "Calculus",
+            "author": "James Stewart",
+            "isbn": "9780538497817",
+            "link": "https://www.goodreads.com/book/show/10663297-calculus-7th-edition?ref=nav_sb_ss_1_13",
+        },
+    ],
+    "Topology": [
+        {
+            "title": "Introduction to Topology",
+            "author": "Bert Mendelson",
+            "isbn": "9780486663524",
+            "link": "https://www.goodreads.com/book/show/116420.Introduction_to_Topology",
+        },
+    ],
+    "Number Theory": [
+        {
+            "title": "An Introduction to the Theory of Numbers",
+            "author": "G. H. Hardy, E. M. Wright",
+            "isbn": "9780199219865",
+            "link": "https://www.goodreads.com/book/show/585623.An_Introduction_to_the_Theory_of_Numbers",
+        },
+        {
+            "title": "Elementary Number Theory",
+            "author": "David M. Burton",
+            "isbn": "9780387901633",
+            "link": "https://www.goodreads.com/book/show/241408.Introduction_to_Analytic_Number_Theory",
+        },
+    ],
+    "Real Analysis": [
+        {
+            "title": "Principles of Mathematical Analysis",
+            "author": "Walter Rudin",
+            "isbn": "9780070542358",
+            "link": "https://www.goodreads.com/book/show/292079.Principles_of_Mathematical_Analysis",
+        },
+    ],
+    "Complex Analysis": [
+        {
+            "title": "Complex Analysis",
+            "author": "Lars Ahlfors",
+            "isbn": "9780070006577",
+            "link": "https://www.goodreads.com/book/show/2437190.Complex_Analysis",
+        },
+    ],
+    "Linear Algebra": [
+        {
+            "title": "Linear Algebra Done Right",
+            "author": "Sheldon Axler",
+            "isbn": "9783319110790",
+            "link": "https://www.goodreads.com/book/show/24848812-linear-algebra-done-right",
+        },
+    ],
+    "Probability and Statistics": [
+        {
+            "title": "An Introduction to Probability Theory and Its Applications",
+            "author": "William Feller",
+            "isbn": "9780471257080",
+            "link": "https://www.goodreads.com/book/show/2378167.An_Introduction_to_Probability_Theory_and_Its_Applications_Volume_1",
+        },
+    ],
+    "Graph Theory": [
+        {
+            "title": "Graph Theory",
+            "author": "Richard J. Trudeau",
+            "isbn": "9780486678702",
+            "link": "https://www.goodreads.com/book/show/388049.Introduction_to_Graph_Theory",
+        },
+    ],
+    "OTHER": [
+        {
+            "title": "eulers gems",
+            "author": "David S. Richeson",
+            "isbn": "9780691126777",
+            "link": "https://www.goodreads.com/book/show/5182228-euler-s-gem",
+        },
+        {
+            "title": "What is Mathematics",
+            "author": "Richard Courant, Ian Stewart, Herbert Robbins",
+            "isbn": "9780195105193",
+            "link": "https://www.goodreads.com/book/show/584620.What_Is_Mathematics_An_Elementary_Approach_to_Ideas_and_Methods",
+        },
+    ]
+}
