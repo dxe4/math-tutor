dfb070eabefc0cae3e3bbb78feec7f2f2da13f61
charalmapos papaloizou <harry@e2e4.email>
2025-02-16 06:50:00 +0000

    add scraping curves

--- math_tutor_backend/math_content/management/commands/import_curves.py
+++ math_tutor_backend/math_content/management/commands/import_curves.py
@@ -3,16 +3,16 @@ from typing import List
 import requests
 from django.core.management.base import BaseCommand
 
-from math_content.models import StAndrewsBiography
+from math_content.models import StAndrewsCurve
 from math_content.scraping import (
-    get_mathematician_info
+    get_links_and_text
 )
 
 URL = "https://mathshistory.st-andrews.ac.uk/Biographies/chronological/"
 
 
 class Command(BaseCommand):
-    help = "Import st Andrew biographics"
+    help = "Import st Andrew curves"
 
     def handle(self, *args, **options):
         self.stdout.write("starting scraping of st andrew topics")
@@ -21,7 +21,11 @@ class Command(BaseCommand):
         if response.status_code != 200:
             raise Exception(f"failed to fetch url {URL}")
 
-        biobraphy_objects = get_mathematician_info(response.text, URL)
-        self.stdout.write(f"found {len(biobraphy_objects)} mathematicians")
+        links = get_links_and_text(response.text, "#main li a", URL)
+        self.stdout.write(f"found {len(links)} curves")
         self.stdout.flush()
-        StAndrewsBiography.objects.bulk_create(biobraphy_objects)
+        curves = [
+            StAndrewsCurve(**{"title": i.text, "link": i.link})
+            for i in links
+        ]
+        StAndrewsCurve.objects.bulk_create(curves)
