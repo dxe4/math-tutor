1d3fb536869b80f1d9285d465e42ca2a5ead6636
charalmapos papaloizou <harry@e2e4.email>
2025-02-21 17:48:18 +0000

    parameterise collatz

--- math_tutor_backend/math_content/management/commands/import_collatz.py
+++ math_tutor_backend/math_content/management/commands/import_collatz.py
@@ -7,11 +7,43 @@ URL = "https://mathshistory.st-andrews.ac.uk/Curves/"
 
 
 class Command(BaseCommand):
-    help = "Import st Andrew curves"
+    help = "Import Collatz"
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--start',
+            type=int,
+            default=1,
+            help='Starting value (default: 1)',
+        )
+        parser.add_argument(
+            '--end',
+            type=int,
+            default=2000,
+            help='Ending value (default: 2000)',
+        )
 
     def handle(self, *args, **options):
         new_objects = []
-        for i in range(1, 2000):
+        start = options['start']
+        end = options['end']
+        if end < start:
+            self.stdout.write(self.style.ERROR(
+                f"end must be bigger than start. \n"
+                "start: {start} end: {end}")
+            )
+            self.stdout.flush()
+            return
+
+        created_in_range = CollatzConjecture.objects.filter(
+            start_number__gte=start,
+            start_number__lte=end,
+        ).values_list("start_number", flat=True)
+        created_in_range = set(created_in_range)
+
+        for i in range(start, end + 1):
+            if i in created_in_range:
+                continue
             collatz = manifold_rs.collatz_sequence(i)
             sequence = collatz.get_seq_str()
             two_adic_distances = collatz.two_adic_disntace_str()
